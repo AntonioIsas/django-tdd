b34d0e8dc43051e38d16fc9243e68359e77e76c8
Antonio Isas <antonioisas87@gmail.com>
2019-11-20 22:58:56 -0600

    Add support for testing staging

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import WebDriverException
 
+from .server_tools import reset_database
+
 MAX_WAIT = 3
 
 
@@ -28,9 +30,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         options.headless = True
         self.browser = webdriver.Firefox(options=options)
 
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
